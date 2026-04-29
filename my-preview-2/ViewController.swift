37f74725512c804750f22dd422929809d656f4bf
fuziki <33706353+fuziki@users.noreply.github.com>
2026-04-29 18:51:34 +0900

    Xcode Template

---
+++ my-preview-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-preview-2
+//
+//  Created by fuziki on 2026/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
